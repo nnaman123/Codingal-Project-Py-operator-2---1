e66bf1b2ad90d972378c86f9144910292490215a
nnaman123 <104630028+nnaman123@users.noreply.github.com>
2025-07-25 21:03:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-a=10
-b=12
-c=0
+#BMI CHECKER
 
-if a and b and c:
-    print("All the numbers have boolean value as True")
+input_weight = float(input("Enter your weight in kg: "))
+input_height = float(input("Enter your height in meters: "))
+
+BMI = input_weight / (input_height ** 2)
+
+print("Your BMI is:", BMI)
+
+if BMI < 18.4:
+    print("You are underweight")
+elif 18.5 <= BMI < 24.9:
+    print("You have a normal weight")
+elif 25 <= BMI < 29.9:
+    print("You are overweight")
+elif 30 <= BMI < 34.9:
+    print("You are obese (Class 1)")
+elif 35 <= BMI < 39.9:
+    print("You are obese (Class 2)")
 else:
-    print("At least one of the numbers has boolean value as False")
-
-
-    a=10
-    b=-10
-    c=0
-    if a > 0 or b > 0:
-        print("At least one of the numbers is positive")
-    else:
-        print("Both numbers are non-positive")
-
-
-    if b > 0 or c > 0:
-            print("At least one of the numbers is positive")
-    else:
-            print("Both numbers are non-positive")
-
-
+    print("You are obese (Class 3)")    
 
